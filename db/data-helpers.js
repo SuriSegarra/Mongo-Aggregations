873bdf2b090452006e35973acd471fe35e694710
surelis segarra <sdarianne34@gmail.com>
2020-03-25 21:53:09 -0700

    gets all author is not passing, it saying getAuthor is not a function

--- db/data-helpers.js
+++ db/data-helpers.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const connect = require('../lib/utils/connect')
+const connect = require('../lib/utils/connect');
 const seed = require('../db/seed');
 const mongoose = require('mongoose');
 const fs = require('fs');
@@ -8,34 +8,35 @@ const fs = require('fs');
 beforeAll(() => {
   connect();
 });
-  
 beforeEach(() => {
   return mongoose.connection.dropDatabase();
 });
-  
 beforeEach(() => {
   return seed();
 });
-  
 afterAll(() => {
   return mongoose.connection.close();
 });
 
 const prepare = model => JSON.parse(JSON.stringify(model));
 const prepareAll = models => models.map(prepare);
-
-//reading our models directory
-const files = fs.readdirSync('../lib/models');
+// reading our models directory
+const files = fs.readdirSync('./lib/models');
 const getters = files
+  // for each file in our models directory import the model
   .map(file => require(`../lib/models/${file}`))
+  // make sure that what we imported is actually a model
   .filter(Model => Model.prototype instanceof mongoose.Model)
+  // for each model create a getModelName function that returns an instance of our model
   .reduce((acc, Model) => {
     return {
       ...acc,
       [`get${Model.modelName}`]: query => Model.findOne(query).then(prepare),
+      //gets books or authors plural.
       [`get${Model.modelName}s`]: query => Model.find(query).then(prepareAll)
     };
-  });
+  }, {});
 
-module.exports = getters;
-
+module.exports = {
+  getters
+};
--- db/seed.js
+++ db/seed.js
@@ -1,19 +1,15 @@
-const Author = require('../lib/models/Authors');
-const Books = require('../lib/models/Books');
+const Author = require('../lib/models/Author');
+const Book = require('../lib/models/Books');
 const chance = require('chance').Chance();
 
 module.exports = async({ authorsToCreate = 5, booksToCreate = 10 } = {}) => {
-
-  const names = ['Malcolm-Gladwell', 'Jane Austen', 'Charlotte Bronte', 'Thomas Hardy'];
+  const name = ['Malcolm-Gladwell', 'Gabriel-Garcia-Marques', 'Random-Author', 'I am out of names'];
   const authors = await Author.create([...Array(authorsToCreate)].map(() => ({
-    name: chance.pickone(names),
-    authorName: chance.sentence()
+    name: chance.pickone(name),
   })));
-  
-  await Books.create([...Array(booksToCreate)].map(() => ({
+  await Book.create([...Array(booksToCreate)].map(() => ({
     authorId: chance.pickone(authors)._id,
-    name: chance.pickone(names),
-    authorName: chance.sentence()
+    title: chance.animal(),
   })));
 };
 
